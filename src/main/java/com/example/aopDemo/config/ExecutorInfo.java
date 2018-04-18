63fd39c10b96787e6b8f94182388820e2280c970
GeniusGeng <dandise22@gmail.com>
2018-04-18 22:22:05 +0800

    Update ExecutorInfo.java

--- src/main/java/com/example/aopDemo/config/ExecutorInfo.java
+++ src/main/java/com/example/aopDemo/config/ExecutorInfo.java
@@ -1,7 +1,10 @@
 package com.example.aopDemo.config;
 
 import java.lang.annotation.*;
-
+/**
+*创建一个自定义的注解
+*Creating a custom annotation
+*/
 @Target({ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExecutorInfo {
